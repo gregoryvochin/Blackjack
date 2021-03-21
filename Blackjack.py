3930b949be1e9b06f852c5bf8220cb434dae5422
Gregory Vochin <gvochin3@gmail.com>
2021-03-20 22:24:49 -0400

    defined more functions for use in the program

--- Blackjack.py
+++ Blackjack.py
@@ -3,8 +3,10 @@ import random
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 user_cards = []
+user_total = 0
 
 comp_cards = []
+comp_total = 0
 
 def deal_cards():
     for i in range(0, 2):
@@ -14,10 +16,20 @@ def deal_cards():
 def hit():
     user_cards.append(random.choice(cards))
 
-deal_cards()
-print(user_cards)
-print(comp_cards)
-hit()
+def user_total():
+    user_total = sum(user_cards)
+    return user_total
 
-print(user_cards)
-print(comp_cards)
+def comp_total():
+    comp_total = sum(comp_cards)
+    return comp_total
+
+deal_cards()
+
+if user_total() > 21:
+    print(f"Your total is over 21 with {user_total}. Bust!")
+
+else:
+    print(user_total())
+    
+print(comp_total())
