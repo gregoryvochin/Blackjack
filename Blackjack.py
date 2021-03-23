8536670fe6fafb48d16b7b35f946529a87027d34
Gregory Vochin <gvochin3@gmail.com>
2021-03-23 08:22:12 -0400

    coded recursive blackjack function, still need to implement dealer code and stand code

--- Blackjack.py
+++ Blackjack.py
@@ -1,5 +1,7 @@
 import random
 
+#plays = {hit: hit(), stand: stand()
+
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 user_cards = []
@@ -13,8 +15,14 @@ def deal_cards():
         user_cards.append(random.choice(cards))
         comp_cards.append(random.choice(cards))
 
+    print(f"You were dealt {user_cards}")
+    print(f"The computer was dealt [?, {comp_cards[1]}]")
+
 def hit():
     user_cards.append(random.choice(cards))
+
+def stand():
+    print("You end your turn!")
 
 def user_total():
     user_total = sum(user_cards)
@@ -24,20 +32,47 @@ def comp_total():
     comp_total = sum(comp_cards)
     return comp_total
 
-user_cards = [11, 10, 2]
+deal_cards()
 
-if user_total() > 21:
-    if 11 in user_cards:
-        for i in range(0, len(user_cards)):
-            if user_cards[i] == 11:
-                print(user_cards)
-                user_cards[i] = 1
-                print(user_cards)
-                
-    print(f"Your total is over 21 with {user_total}. Bust!")
+def blackjack(user_total = user_total, comp_total = comp_total):
+    winOrLose = None
+    hitOrStand = None
+    
+    user_total = user_total()
+    comp_total = comp_total()
+    
+    if user_total == 21:
+        print("Blackjack!")
+        if comp_total == 21:
+            print("Push!")
+            return winOrLose
+        else:
+            print("You win!")
+            winOrLose = True
+            return winOrLose
 
-else:
-    print(user_total())
+    elif user_total > 21:
+        if 11 in user_cards:
+            for card in range(0, len(user_cards)):
+                if user_cards[card] == 11:
+                    user_cards[card] = 1
+                    print(f"You went over 21, but had an Ace! Your new cards are {user_cards}")
+                    blackjack()
+        else:
+            print(f"Bust! Your total ({user_total}) went over 21!")
+            winOrLose = False
+            return winOrLose
+
+    else:
+        
+        hitOrStand = input("Enter 'hit' to get a card, or 'stand' to keep what you have and let the dealer play.")
+        hitOrStand = hitOrStand.lower()
+        if hitOrStand == "hit":
+            hit()
+            print(user_cards)
+            blackjack()
+
+blackjack()
 
 print(user_total())
 print(comp_total())
